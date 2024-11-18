0380b0dbc7599b48d65876fdbdb2051fde7c87ee
Idzard Kwadijk <kwadijk@idzard.com>
2024-11-18 03:23:49 +0100

    2 steppers and homing working

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,10 @@
 #define stepPinStepper1 15
 #define dirPinStepper1 14
 
-#define stepPinStepper2 15
-#define dirPinStepper2 14
+#define stepPinStepper2 32
+#define dirPinStepper2 33
 
 #define DRIVER_MCPWM_PCNT 0
-
 
 long m1Max = 3200;
 long m2Max = 3200;
@@ -20,6 +19,7 @@ struct stepper_config_s {
   uint8_t step;
   uint8_t direction;
   uint32_t speed;
+  uint32_t homingSpeed;
   uint32_t acceleration;
 };
 
@@ -27,13 +27,15 @@ const struct stepper_config_s stepper_config[2] = {
     {
       step : stepPinStepper1,
       direction : dirPinStepper1,
-      speed: 30000,
+      speed: 6000,
+      homingSpeed: 3000,
       acceleration: 20000
     },
     {
       step : stepPinStepper2,
       direction : dirPinStepper2,
-      speed: 30000,
+      speed: 6000,
+      homingSpeed: 3000,
       acceleration: 20000
     }};
 
@@ -52,7 +54,7 @@ uint8_t net = 0;        // 0 - 127
 uint8_t subnet = 0;     // 0 - 15
 uint8_t universe2 = 2;  // 0 - 15
 
-unsigned long last = 0;
+unsigned long lastHeartbeat = 0;
 
 void onArtnetReceive(const uint8_t *data, uint16_t size, const ArtDmxMetadata &metadata, const ArtNetRemoteInfo &remote);
 void homeSteppers();
@@ -63,7 +65,6 @@ void setup() {
   
   ETH.begin();
   ETH.config(ip, gateway, subnet_mask);
-
   MDNS.begin("window1"); 
   
   limitSwitch.setDebounceTime(50); // set debounce time to 50 milliseconds
@@ -82,30 +83,30 @@ void setup() {
     }
     stepper[i] = s;
   }
-
+  
   artnet.begin();
   artnet.subscribeArtDmxUniverse(net, subnet, universe1, onArtnetReceive);
 
-  //homeSteppers();
+  homeSteppers();
 }
 
 void loop() {
   limitSwitch.loop();
   
   if(limitSwitch.isPressed())
+  {
     stepper[0]->forceStop();
-    Serial.println("Stepper0 is homed");
-    
-
+    Serial.println("Stepper0 is homed at:");
+    Serial.println(stepper[0]->getCurrentPosition());
+  }
   if(limitSwitch.isReleased())
     Serial.println("The button is released");
 
-
   artnet.parse();  // check if artnet packet has come and execute callback function
   
-  if (millis() > last + 1000){
+  if (millis() > lastHeartbeat + 1000){
       Serial.print(".");
-      last = millis();
+      lastHeartbeat = millis();
     }
 }
 
@@ -129,6 +130,7 @@ void onArtnetReceive(const uint8_t *data, uint16_t size, const ArtDmxMetadata &m
     Serial.print("M2: ");
     Serial.print(m2);
     Serial.println("");
+
     
     Serial.println("Art-Net data received:");
     Serial.print(remote.ip);
@@ -142,15 +144,19 @@ void onArtnetReceive(const uint8_t *data, uint16_t size, const ArtDmxMetadata &m
 
     uint16_t m1Remapped = map(m1, 0, 65535, 0, m1Max);
     uint16_t m2Remapped = map(m2, 0, 65535, 0, m2Max);
-    Serial.print("moving stepper to: ");
+    Serial.print("moving stepper0 to: ");
     Serial.print(m1Remapped);
     Serial.println();
-    
+    Serial.print("moving stepper1 to: ");
+    Serial.print(m2Remapped);
+    Serial.println();
     stepper[0]->moveTo(m1Remapped);
-
+    stepper[1]->moveTo(m2Remapped);
 }
 
 
 void homeSteppers() {
+  stepper[0]->setCurrentPosition(0);
+  stepper[0]->setSpeedInHz(stepper_config[0].homingSpeed);
   stepper[0]->runForward();
 }
